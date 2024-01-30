6a3040533f346a2ff0948598f8fe4ad4eb94023f
igorbokun2022 <116080243+igorbokun2022@users.noreply.github.com>
2024-01-30 17:16:42 +0500

    Add files via upload

--- test_2.py
+++ test_2.py
@@ -4,10 +4,11 @@ import PIL as pil
 import httpx
 import asyncio
 import feedparser
+from collections import deque
 import threading
 
 
-flagLocal=False
+flagLocal=True
 if flagLocal==True: path='F:/_Data Sience/Веб_приложения/Streamlit/demo_test_2/'
 else:               path=''
 
@@ -31,43 +32,43 @@ async def rss_parser(httpx_client, posted_q,
         except:
             await asyncio.sleep(10)
             continue
-
+                    
         feed = feedparser.parse(response.text)
 
         for entry in feed.entries[::-1]:
-            summary = entry['summary']
-            title = entry['title']
-
-            news_text = f'{title}\n{summary}'
-
-            head = news_text[:n_test_chars].strip()
-
-            if head in posted_q:
-                continue
-
-            if send_message_func is None:
-                #print(news_text, '\n')
-                st.text(news_text)
-            else:
-                await send_message_func(f'rbc.ru\n{news_text}')
-           
-            curcntmes=curcntmes+1 
-            posted_q.appendleft(head)
-        
+                    
+            curcntmes=curcntmes+1
+            print(curcntmes)
+            
             if curcntmes>maxcntmes:
-                asyncio.stop()
-                thread1.stop()
+                print("STOP!")
                 flagCycle=False
                 exit
                 
+            summary = entry['summary']
+            title = entry['title']
+            news_text = f'{title}\n{summary}'
+            head = news_text[:n_test_chars].strip()
+
+            if head in posted_q: continue
+            posted_q.appendleft(head)
+
+            if send_message_func is None:
+                print(news_text, '\n')
+                st.text(curcntmes)
+                st.text(news_text)
+            else:
+                await send_message_func(f'rbc.ru\n{news_text}')
+                                      
         await asyncio.sleep(5)
     
 def go():
-    print("11111111111111111111111111111")
+    print(" *** START GO *** ")
     asyncio.run(rss_parser(httpx_client, posted_q, n_test_chars))
-    print("55555555555555555555555555555")
+    print("*** STOP GO *** ")
 
 #----------------------------------------------------------------------------
+# Очередь из уже опубликованных постов, чтобы их не дублировать
 # Очередь из уже опубликованных постов, чтобы их не дублировать
 posted_q = deque(maxlen=20)
 # 50 первых символов от текста новости - это ключ для проверки повторени
