f0528ed7bce58fc542e5f64576237bce5086278c
igorbokun2022 <116080243+igorbokun2022@users.noreply.github.com>
2024-04-26 13:36:50 +0500

    Add files via upload

--- test_2.py
+++ test_2.py
@@ -1,9 +1,4 @@
 import streamlit as st 
 import PIL as pil 
 
-st.header('Web-сервис: тематичеcкий онлайн анализ контента новостных каналов')
-try:
-    img=pil.Image.open('photo.jpg')
-except:
-    img=pil.Image.open('F:/_Data Sience/Веб_приложения/Streamlit/demo_test_1/photo.jpg')    
-st.sidebar.image(img, width=250)
+st.header('Web-сервис на основе python (streamlit)')
