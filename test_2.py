40b3a09d73bc387221e67067188c7210fea82483
igorbokun2022 <116080243+igorbokun2022@users.noreply.github.com>
2024-04-26 17:08:17 +0500

    Add files via upload

--- test_2.py
+++ test_2.py
@@ -1,4 +1,12 @@
 import streamlit as st 
 import PIL as pil 
+import datetime as dt
 
 st.header('Web-сервис на основе python (streamlit)')
+st.info(st.server.request)
+
+if st.server.request == 'bar':
+    st.text('********************************************')
+    #df=dt.time()     
+    #st.server.({'data': df})
+  # And .respond() would cause the script to finish executing immediately.
