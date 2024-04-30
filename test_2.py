6d1bc14b99569eee3b693a1e6520197725e4054f
igorbokun2022 <116080243+igorbokun2022@users.noreply.github.com>
2024-04-30 14:21:37 +0500

    Add files via upload

--- test_2.py
+++ test_2.py
@@ -4,14 +4,13 @@ import datetime as dt
 
 st.header('Web-сервис на основе python (streamlit)')
 
-if "messages" not in st.session_state:
-    st.text('********************************************')
-    st.text("st.session_state: "+str(st.session_state))
-    st.text('********************************************')
-    st.text(dt.time())    
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-    for message in st.session_state.messages:   
-        st.text(message)
+params = st.experimental_get_query_params()
+
+st.text('********************************************')
+param1 = params['param1'][0] if 'param1' in params else None
+#param2 = params['param2'][0] if 'param2' in params else None
+st.info('Param1:'+ str(param1))
+st.text('********************************************')
+st.text(dt.time())    
             
     
